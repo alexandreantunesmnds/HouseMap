c025af34e0873a431e7b732c7b0aacf771cc8bf9
Alex <alexmendes74@gmail.com>
2022-10-25 17:01:13 +0200

    Ajout de la classe batiment

--- app/src/main/java/com/example/housemap/Batiment.java
+++ app/src/main/java/com/example/housemap/Batiment.java
@@ -1,4 +1,20 @@
 package com.example.housemap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Batiment {
+    private String nomBat;
+    private List<Piece> listPieces;
+    public Batiment() {
+        nomBat = "Batiment 1";
+        listPieces = new ArrayList<>(10);
+    }
+    private void ajouterPiece(Piece piece){
+        listPieces.add(piece);
+    }
+
+    private int getNbPieces(Piece piece){
+        return listPieces.size();
+    }
 }
