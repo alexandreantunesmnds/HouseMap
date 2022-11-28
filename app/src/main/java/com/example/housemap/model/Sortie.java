df1bf76e5ccd8a254fa5cddedfd175fefab6d522
Alex <alexmendes74@gmail.com>
2022-11-28 13:29:29 +0100

    Ajout de la classe Sortie

---
+++ app/src/main/java/com/example/housemap/model/Sortie.java
@@ -0,0 +1,15 @@
+package com.example.housemap.model;
+
+import android.graphics.Rect;
+
+public class Sortie {
+    Piece piece;
+    int noSortie;
+    Rect zoneSortie;
+
+    public Sortie(Piece piece, int noSortie, Rect zoneSortie) {
+        this.piece = piece;
+        this.noSortie = noSortie;
+        this.zoneSortie = zoneSortie;
+    }
+}
