ce30c26d7b3de258f814ba292912c5ead4d0fde1
Alex <alexmendes74@gmail.com>
2022-12-04 18:34:00 +0100

    Travail sur la modification des murs

--- app/src/main/java/com/example/housemap/ChangerActivity.java
+++ app/src/main/java/com/example/housemap/ChangerActivity.java
@@ -1,21 +1,29 @@
 package com.example.housemap;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.housemap.model.Batiment;
+import com.example.housemap.model.Mur;
 import com.example.housemap.model.Piece;
 
 public class ChangerActivity extends AppCompatActivity {
     private Piece piece;
     private Batiment maison;
+    private EditText nomPiece;
+    private String nomPieceS;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_changer);
+        nomPiece = findViewById(R.id.editTextTextNamePiece3);
         if(getIntent().getExtras() != null) {
             piece = (Piece) getIntent().getSerializableExtra("piece");
             maison = (Batiment) getIntent().getSerializableExtra("maison"); //on récupère le batiment créer
@@ -29,5 +37,77 @@ public class ChangerActivity extends AppCompatActivity {
         intent.putExtra("maison",maison);
         setResult(RESULT_OK, intent);
         finish();
+    }
+    public void clickValiderNom(View view) {
+        if(!(nomPiece.getText().toString().equals(""))){//si on a écrit quelque chose
+            nomPieceS = nomPiece.getText().toString();
+            piece.setNom(nomPieceS);
+            Toast.makeText(ChangerActivity.this, "Nom de la pièce modifié avec succès", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(ChangerActivity.this, "Veuillez saisir un nom valide", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void clickValider(View view) {
+    }
+
+    public void clickNord(View view) {
+        Intent intent = new Intent(ChangerActivity.this, ModifHouseActivity.class);
+        Bundle extras = new Bundle();
+        Mur nord = piece.getMur(0);
+        extras.putSerializable("mur",nord);
+        extras.putSerializable("piece",piece);
+        extras.putSerializable("maison",maison);
+        intent.putExtras(extras);
+        startActivityForResult(intent,12);
+    }
+
+    public void clickOuest(View view) {
+        Intent intent = new Intent(ChangerActivity.this, ModifHouseActivity.class);
+        Bundle extras = new Bundle();
+        Mur ouest = piece.getMur(3);
+        extras.putSerializable("mur",ouest);
+        extras.putSerializable("piece",piece);
+        extras.putSerializable("maison",maison);
+        intent.putExtras(extras);
+        startActivityForResult(intent,12);
+    }
+
+    public void clickSud(View view) {
+        Intent intent = new Intent(ChangerActivity.this, ModifHouseActivity.class);
+        Bundle extras = new Bundle();
+        Mur sud = piece.getMur(1);
+        extras.putSerializable("mur",sud);
+        extras.putSerializable("piece",piece);
+        extras.putSerializable("maison",maison);
+        intent.putExtras(extras);
+        startActivityForResult(intent,12);
+    }
+
+    public void clickEst(View view) {
+        Intent intent = new Intent(ChangerActivity.this, ModifHouseActivity.class);
+        Bundle extras = new Bundle();
+        Mur est = piece.getMur(2);
+        extras.putSerializable("mur",est);
+        extras.putSerializable("piece",piece);
+        extras.putSerializable("maison",maison);
+        intent.putExtras(extras);
+        startActivityForResult(intent,12);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // check that it is the SecondActivity with an OK result
+        if (requestCode == 12) {
+            if (resultCode == RESULT_OK) {//mise à jour de la maison et de la piece
+                Batiment maison = (Batiment) data.getSerializableExtra("maison");
+                Piece piece = (Piece) data.getSerializableExtra("piece");
+                this.maison = maison;
+                this.piece = piece;
+            }
+        }
     }
 }
