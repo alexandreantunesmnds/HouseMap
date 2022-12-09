4f9dccf94cbdeebcfdeb022ef67098ba7a62022f
Alex <alexmendes74@gmail.com>
2022-12-09 19:20:17 +0100

    a faire la fonctionnalite des portes dans la visu

--- app/src/main/java/com/example/housemap/VisualisationActivity.java
+++ app/src/main/java/com/example/housemap/VisualisationActivity.java
@@ -2,6 +2,7 @@ package com.example.housemap;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.view.View;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,16 +16,24 @@ import java.io.FileOutputStream;
 public class VisualisationActivity extends AppCompatActivity {
     private Piece piece;
     private Batiment maison;
+    private int i;
+
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualisation);
+        i = 0;
+        img = findViewById(R.id.imageView);
         if(getIntent().getExtras() != null) {
             piece = (Piece) getIntent().getSerializableExtra("piece");
             maison = (Batiment) getIntent().getSerializableExtra("maison"); //on récupère le batiment créer
-            ImageView img = findViewById(R.id.imageView);
-            String nameFile = piece.getMur(0).getNomPhoto();
+        }
+        afficheImage(0); //on affiche le mur nord de base
+    }
+    public void afficheImage (int i){
+            String nameFile = piece.getMur(i).getNomPhoto();
             FileInputStream fis;
             try {
                 fis = openFileInput(nameFile);
@@ -35,6 +44,26 @@ public class VisualisationActivity extends AppCompatActivity {
             bm = Bitmap.createScaledBitmap(bm, 1000, 1000, false);
             // Reste à mettre bm à mettre sur l’ImageView
             img.setImageBitmap(bm);
+
+    }
+
+    public void clickSuivant(View view) {
+        if(i<3) {
+            i++;
+            afficheImage(i);
+        }
+        else{
+            i=0;
+        }
+    }
+
+    public void clickPrec(View view) {//a revoir cette fonction
+        if(i>0) {
+            i--;
+            afficheImage(i);
+        }
+        else{
+            i=0;
         }
     }
 }
